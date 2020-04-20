e0141476a75d6cbe89cb28a39cadc80b9f41359b
leoherz <leoherz_liu@163.com>
2020-04-20 16:52:32 +0800

    rename some variables

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from optics import OPTICS
 
-COLORS = ['gold', 'darkorange', 'lightcoral',
-          'seagreen', 'c', 'steelblue', 'slateblue']
+COLORS = ['plum', 'palevioletred', 'sandybrown', 'lightcoral', 'crimson',
+          'mediumaquamarine', 'steelblue', 'slateblue', "cadetblue", 'darkcyan'
+          'cornflowerblue', 'seagreen', 'lightsalmon', 'burlywood', 'forestgreen']
 
 if __name__ == "__main__":
     df = pd.read_table("cluster_data_set/Aggregation.txt",
@@ -30,33 +31,39 @@ if __name__ == "__main__":
     index = np.linspace(0, model.len, model.len)
     rd = []
     cd = []
-    category = []
+    labels = []
     for i in range(model.len):
         rd.append(model.reachable_distances[model.result_queue[i]])
         cd.append(model.core_distances[model.result_queue[i]])
-        category.append(
+        labels.append(
             COLORS[df.iloc[model.result_queue[i], 2] % len(COLORS)])
-    plt.bar(index, rd, color=category)
-    plt.plot(index, rd, color='black', linewidth=0.5)
+    plt.bar(index, rd, color=labels)
+    plt.plot(index, rd, color='black', linewidth=0.3)
     plt.ylim(0, 5)
     plt.xlim(-5, model.len+5)
     fig2 = plt.figure("ordered rd and cd")
-    plt.fill_between(index, 0, cd, alpha=0.3, color='lightcoral')
-    plt.fill_between(index, 0, rd, alpha=0.3, color='slateblue')
+    plt.fill_between(index, 0, cd, alpha=0.25, color='crimson')
+    plt.fill_between(index, 0, rd, alpha=0.2, color='cornflowerblue')
     plt.xlim(-5, model.len+5)
     plt.ylim(0, 5)
 
     # extract cluster result and visualize
-    model.cluster_extract(1.65)
+    model.cluster_extract(1.6)
     fig3 = plt.figure("clustering result")
-    cluster = []
+    cluster_labels = []
     for i in range(model.len):
-        c = model.category_queue[model.result_queue[i]]
+        c = model.cluster_labels[i]
         if c == -1:
-            cluster.append("black")
+            cluster_labels.append("darkgrey")
         else:
-            cluster.append(
+            cluster_labels.append(
                 COLORS[c % len(COLORS)])
-    plt.scatter(df.iloc[:, 0], df.iloc[:, 1], color=cluster)
+    plt.scatter(df.iloc[:, 0], df.iloc[:, 1], color=cluster_labels)
+
+    fig4 = plt.figure("rd with clustering labels")
+    plt.bar(index, rd, color=[cluster_labels[i] for i in model.result_queue])
+    plt.plot(index, rd, color='black', linewidth=0.3)
+    plt.xlim(-5, model.len + 5)
+    plt.ylim(0, 5)
 
     plt.show()
